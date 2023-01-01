ce885181f68e2ad31b2a5e6e244e768eb004ec48
wecreate <2633132826@qq.com>
2023-01-01 23:02:15 +0800

    开发日志模块

--- src/views/Course/index.vue
+++ src/views/Course/index.vue
@@ -1,18 +1,45 @@
 <template>
-    <div class="margin-main-top">
-        <img src="https://ghchart.rshah.org/Chuang0516" />
+    <div class="development-logs">
+        <img class="githubContribute" src="https://ghchart.rshah.org/Chuang0516">
+        <TimeLine :timelineList="timelineList" />
     </div>
 </template>
 
 <script>
-
+import TimeLine from '@/views/Course/TimeLine'
 export default {
-
+    name: 'Course',
+    components: { TimeLine },
+    data() {
+        return {
+            timelineList: []
+        }
+    },
+    methods: {
+        getGithubCommit() {
+            fetch('https://api.github.com/repos/Chuang0516/CreateCoder/commits')
+                .then(res => res.json())
+                .then(data => this.timelineList = data)
+        }
+    },
+    mounted() {
+        this.getGithubCommit()
+    }
 }
 </script>
 
-<style>
-.margin-main-top {
+<style lang="less" scoped>
+.development-logs {
     margin-top: 56px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .githubContribute {
+        display: block;
+        width: 80%;
+        margin: 20px auto;
+        -webkit-user-drag: none;
+    }
 }
 </style>
